bb4024c32829e754ff8c95db9ec4da1359105a97
SidCode96 <sidchandra50@gmail.com>
2023-03-15 01:51:20 +0530

    End of Section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public bool IsActive { get; set; }
+       
+
+    }
+}
